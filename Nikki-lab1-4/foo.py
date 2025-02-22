9c45619b660078543cd3e5dd471ed3c886cfb743
Lei Ningyu <3232254146@qq.com>
2025-02-22 13:35:51 +0300

    更新内容

--- Nikki-lab1-4/foo.py
+++ Nikki-lab1-4/foo.py
@@ -1,9 +1,122 @@
-class Foo(object):
-    def hello(self):
-        return "hello"
+from functools import reduce
 
-    def add(self, a, b):
-        return a + b
+class HashSet:
+    DEFAULT_CAPACITY = 16
 
-    def sub(self, a, b):
-        return a - b
+    def __init__(self, capacity=DEFAULT_CAPACITY):
+        self.capacity = max(1, capacity)
+        self.buckets = [[] for _ in range(self.capacity)]
+        self._size = 0
+
+    def _hash(self, value):
+        """处理None值的哈希函数"""
+        return hash(value) % self.capacity if value is not None else 0
+
+    # 1. 添加元素
+    def add(self, value):
+        idx = self._hash(value)
+        if value not in self.buckets[idx]:
+            self.buckets[idx].append(value)
+            self._size += 1
+
+    # 2. 更新元素（集合特性需先删后加）
+    def set(self, old_value, new_value):
+        if self.member(old_value):
+            self.remove(old_value)
+            self.add(new_value)
+
+    # 3. 删除元素
+    def remove(self, value):
+        idx = self._hash(value)
+        try:
+            self.buckets[idx].remove(value)
+            self._size -= 1
+            return True
+        except ValueError:
+            return False
+
+    # 4. 集合大小
+    def size(self):
+        return self._size
+
+    # 5. 成员判断
+    def member(self, value):
+        return value in self.buckets[self._hash(value)]
+
+    # 6. 反转所有桶内元素顺序
+    def reverse(self):
+        for bucket in self.buckets:
+            bucket.reverse()   
+        self.buckets.reverse()
+
+    # 7. 从列表初始化
+    # 修改from_list方法
+    def from_list(self, lst):
+        for item in lst:
+            self.add(item)
+        return self  # 添加返回语句
+
+
+    # 8. 转换为列表
+    def to_list(self):
+        return [item for bucket in self.buckets for item in bucket]
+
+    # 9. 过滤元素
+    def filter(self, predicate):
+        new_set = HashSet(self.capacity)
+        for bucket in self.buckets:
+            for item in bucket:
+                if predicate(item):
+                    new_set.add(item)
+        return new_set
+
+    # 10. 映射函数
+    def map(self, f):
+        new_set = HashSet(self.capacity)
+        for bucket in self.buckets:
+            for item in bucket:
+                new_set.add(f(item))
+        return new_set
+
+    # 11. 归约操作
+    def reduce(self, reducer, initial=None):
+        it = iter(self)
+        if initial is None:
+            try:
+                value = next(it)
+            except StopIteration:
+                raise TypeError("Reduce of empty set with no initial value")
+        else:
+            value = initial
+        for element in it:
+            value = reducer(value, element)
+        return value
+
+    # 12. 迭代器实现
+    def __iter__(self):
+        for bucket in self.buckets:
+            yield from bucket
+
+    def __next__(self):
+        return next(iter(self))
+
+    # 13. 单位元（Monoid）
+    @classmethod
+    def empty(cls):
+        return cls(1)  # 最小容量
+
+    # 14. 结合操作（Monoid）
+    def concat(self, other):
+        new_set = HashSet()
+        if self:  # 处理空集合
+            new_set.from_list(self.to_list())
+        if other:
+            new_set.from_list(other.to_list())
+        return new_set
+
+    # 特殊方法
+    def __str__(self):
+        return "{" + ", ".join(map(str, self)) + "}"
+
+    def __contains__(self, value):
+        return self.member(value)
--- Nikki-lab1-4/foo_test.py
+++ Nikki-lab1-4/foo_test.py
@@ -1,14 +1,185 @@
-import unittest
-from hypothesis import given,  strategies
+import pytest
+from hypothesis import given, strategies as st
+from foo import HashSet  # 替换为实际模块名
 
-from foo import Foo
+# ---------------------- 基础功能测试 ----------------------
+def test_add():
+    s = HashSet()
+    s.add(1)
+    assert s.size() == 1
+    s.add(1)  # 重复元素
+    assert s.size() == 1
+    s.add(None)
+    assert s.size() == 2
 
+def test_set():
+    s = HashSet()
+    s.add(1)
+    s.set(1, 2)
+    assert s.member(2)
+    assert not s.member(1)
+    assert s.size() == 1
 
-class TestFoo(unittest.TestCase):
+def test_remove():
+    s = HashSet()
+    s.add(1)
+    assert s.remove(1) is True
+    assert s.size() == 0
+    assert s.remove(999) is False  # 不存在的元素
+    s.add(None)
+    assert s.remove(None) is True
 
-    def test_hello(self):
-        self.assertEqual(Foo().hello(), "hello")
+def test_size():
+    s = HashSet()
+    assert s.size() == 0
+    s.add(1)
+    assert s.size() == 1
+    s.add(2)
+    assert s.size() == 2
 
-    @given(strategies.integers(), strategies.integers())
-    def test_add_commutative(self, a, b):
-        self.assertEqual(Foo().add(a, b), Foo().add(b, a))
+def test_member():
+    s = HashSet()
+    s.add(1)
+    assert s.member(1) is True
+    assert s.member(2) is False
+    s.add(None)
+    assert s.member(None) is True
+
+def test_reverse():
+    # 测试单个桶的反转
+    s = HashSet(capacity=1)  # 强制所有元素进同一个桶
+    s.from_list([1, 2, 3])
+    s.reverse()
+    assert s.to_list() == [3, 2, 1]  # 现在可以正确断言
+    
+    # 测试多桶情况
+    s = HashSet(capacity=3)
+    s.from_list([0, 1, 2, 3])
+    s.reverse()
+    assert s.to_list() == [2, 1, 3, 0]
+
+# ---------------------- 转换测试 ----------------------
+def test_from_list_to_list():
+    test_data = [
+        [],
+        [1],
+        [1, 2, 3],
+        [None, "a", 3.14]
+    ]
+    for data in test_data:
+        s = HashSet()
+        s.from_list(data)
+        # 注意集合的无序性
+        assert sorted(s.to_list(), key=hash) == sorted(list(set(data)), key=hash)
+
+# ---------------------- 高阶函数测试 ----------------------
+def test_filter():
+    s = HashSet()
+    s.from_list([1, 2, 3, 4])
+    filtered = s.filter(lambda x: x % 2 == 0)
+    assert sorted(filtered.to_list()) == [2, 4]
+
+def test_map():
+    s = HashSet()
+    s.from_list([1, 2, 3])
+    mapped = s.map(lambda x: x * 2)
+    assert sorted(mapped.to_list()) == [2, 4, 6]
+
+def test_reduce():
+    s = HashSet()
+    assert s.reduce(lambda a,b: a+b, 0) == 0  # 空集合
+    
+    s.from_list([1,2,3])
+    assert s.reduce(lambda a,b: a+b) == 6  # 无初始值
+    assert s.reduce(lambda a,b: a+b, 10) == 16  # 有初始值
+
+# ---------------------- 迭代器测试 ----------------------
+def test_iterator():
+    data = [1, 2, 3]
+    s = HashSet()
+    s.from_list(data)
+    collected = []
+    for x in s:
+        collected.append(x)
+    assert sorted(collected) == sorted(data)
+    
+    empty_iter = iter(HashSet())
+    with pytest.raises(StopIteration):
+        next(empty_iter)
+
+# ---------------------- Monoid测试 ----------------------
+def test_empty():
+    empty = HashSet.empty()
+    assert empty.size() == 0
+    assert empty.to_list() == []
+
+def test_concat():
+    s1 = HashSet().from_list([1, 2])
+    s2 = HashSet().from_list([2, 3])
+    combined = s1.concat(s2)
+    assert sorted(combined.to_list()) == [1, 2, 3]
+    
+    # 空集合测试
+    empty = HashSet.empty()
+    combined = empty.concat(s1)
+    assert combined.to_list() == s1.to_list()
+
+# # ---------------------- 基于属性的测试 ----------------------
+# @given(st.lists(st.one_of(st.integers(), st.text(), st.none())))
+# def test_from_to_list_equivalence(data):
+#     s = HashSet()
+#     s.from_list(data)
+#     # 考虑集合特性和哈希顺序
+#     assert set(s.to_list()) == set(data)
+#     assert len(s.to_list()) == len(set(data))
+
+# @given(st.lists(st.integers()))
+# def test_size_equivalence(data):
+#     s = HashSet()
+#     s.from_list(data)
+#     assert s.size() == len(set(data))
+
+# @given(st.integers(), st.integers())
+# def test_monoid_laws():
+#     # 准备测试数据
+#     a = 1
+#     b = 2
+    
+#     # 单位元测试
+#     empty = HashSet.empty()
+#     s = HashSet().from_list([a])
+#     assert s.concat(empty).to_list() == s.to_list()
+#     assert empty.concat(s).to_list() == s.to_list()
+    
+#     # 结合律测试
+#     s1 = HashSet().from_list([a])
+#     s2 = HashSet().from_list([b])
+#     s3 = s1.concat(s2)
+#     assert sorted(s3.to_list()) == [1, 2]
+    
+#     # None处理测试
+#     s_none = HashSet().from_list([None])
+#     combined = s_none.concat(empty)
+#     assert None in combined.to_list()
+
+# # ---------------------- 边界测试 ----------------------
+# def test_none_handling():
+#     s = HashSet()
+#     s.add(None)
+#     assert s.size() == 1
+#     assert s.member(None) is True
+#     s.remove(None)
+#     assert s.size() == 0
+
+# def test_collision_handling():
+#     # 强制哈希冲突（假设容量为1）
+#     s = HashSet(1)
+#     s.add(1)
+#     s.add("1")
+#     assert s.size() == 2
+#     assert s.member(1) and s.member("1")
+
+# def test_reduce_exception():
+#     s = HashSet()
+#     with pytest.raises(TypeError):
+#         s.reduce(lambda a,b: a+b)  # 空集合无初始值
